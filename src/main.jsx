212281f97905e0f077d03e8d16b236c149389d22
K Pavan <pavanppvss@gmail.com>
2026-01-30 23:52:27 +0530

    Add PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,3 +12,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+registerSW({
+  onNeedRefresh() {
+    console.log('New version available')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  }
+})
